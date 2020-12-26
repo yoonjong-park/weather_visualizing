14f0493addfffdbda72f4dcceebe19a96ba3c551
yoonjong.park <yoonjongpark@gmail.com>
2020-12-27 08:14:51 +0900

    NEW : HTML, JS
    Using Weather api 🌞

---
+++ README.md
@@ -0,0 +1,14 @@
+# weather-visualizing
+
+This code shows a graphic image on the front of the browser depending on the weather.
+
+## How To Use
+
+<ol>
+  <li>https://openweathermap.org</li>
+  <li>Generation API Key</li>
+  <li>Change API Key Value in this CODE.</li>
+  <li>Enjoy to Use</li>
+</ol>
+
+<strong>License : CC-BY-SA</strong>
---
+++ weather.js
@@ -0,0 +1,55 @@
+const weather = document.querySelector('.weather-status');
+
+const COORDS = 'coords';
+const API_KEY = 'ecda71d9eb9ccc98ead88b85eb2b8932';
+
+function getWeather(lat, lon) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const status = json.weather.id;
+      weather.innerText = `${status}`;
+      console.log('날씨 : ' + status);
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude, // = latitude = latitude
+    longitude, // = longitude = longitude
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Can't access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
